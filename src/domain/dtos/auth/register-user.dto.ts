392fff112c4a3023fcd46e16c28da2e9df55ff57
marimorita <marimm0927@gmail.com>
2025-05-07 15:36:00 -0500

    feat(EDUABack):Add registerUserDto, add new paths

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -4,9 +4,9 @@ export class RegisterUserDto {
     private constructor(
         public name: string,
         public email: string,
-        public password: string
+        public password: string,
     ){}
-    static create (object: {[key:string]: any}) : [string?, RegisterUserDto?]{
+    static create (object: {[ key:string ] : any;}) : [string?, RegisterUserDto?]{
         const {name, email, password} = object;
 
         if (!name) return ['Missing name'];
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -4,12 +4,13 @@ import { AuthDataSourceImpl, AuthRepositoryImpl } from "../../infraestructure";
 
 export class AuthRoutes {
     static get routes(): Router {
-        const router = Router();
+        const router = Router(); 
         const datasource = new AuthDataSourceImpl();
         const AuthRepository = new AuthRepositoryImpl(datasource);
         const controller = new AuthController(AuthRepository);
         
         router.post('/login', controller.loginUser)
+
         router.post('/register', controller.registerUser)
         return router;
     }
